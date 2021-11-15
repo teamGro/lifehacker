48de62d5cec3a8ccb7e9af89e2c8feaa05ec146f
TG <tima.groshev@yandex.ru>
2021-11-15 18:12:04 +0300

    added team

--- src/Pages/ResultPage.vue
+++ src/Pages/ResultPage.vue
@@ -15,7 +15,37 @@
         <div class="result__btns-wrapper">
           <social-link-list class="result__social"></social-link-list>
           <button @click="tryAgain" class="result__again-btn">
-            <span class="result__again-icon"></span>
+            <!-- <span ></span> -->
+            <svg
+              class="result__again-icon"
+              width="40"
+              height="36"
+              viewBox="0 0 40 36"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              id="tryAgain"
+            >
+              <path
+                d="M22.7161 0.716064C13.1883 0.716064 5.4403 8.4722 5.4403
+                18V18.0571L2.01944 12.9054C1.6847
+                12.3992
+                0.998893 12.2605 0.492703 12.5952C-0.0134861 12.9299 -0.15228
+                13.6157 0.182458 14.1219L5.14638 21.5923C5.326
+                21.8617 5.61175 22.0332 5.93016 22.074C5.97915 22.0822 6.01997 22.0822
+                6.06895 22.0822C6.33838 22.0822 6.59964
+                21.9842 6.80374 21.7964L13.7026 15.5833C14.1517 15.1751 14.1925 14.4812
+                13.7843 14.024C13.376 13.5749 12.6821
+                13.5341 12.2249 13.9423L7.64467 18.0816V18C7.64467 9.68052 14.4048 2.92044
+                22.7161 2.92044C31.0274 2.92044 37.7956
+                9.68052 37.7956 18C37.7956 26.3195 31.0355 33.0795 22.7242 33.0795C18.6992
+                33.0795 14.9109 31.512 12.0697 28.6626C11.637
+                28.2299 10.9431 28.2299 10.5104 28.6626C10.0776 29.0953 10.0776 29.7893 10.5104
+                30.222C13.7761 33.4878 18.1114 35.2839
+                22.7242 35.2839C32.2439 35.2839 40 27.5359 40 18C40 8.46403 32.2439
+                0.716064 22.7161 0.716064Z"
+                fill="#7DBFFF"
+              />
+            </svg>
             <span class="result__again-text">Пройти ещё раз</span>
           </button>
         </div>
@@ -29,7 +59,22 @@
         </div>
       </section>
       <div class="result__footer container">
-        <a href="#" class="link result__team">Команда проекта</a>
+        <div class="result__team team">
+          <a
+            href="#"
+            class="link result__team"
+            @mouseover="showTeam"
+            @mouseleave="hideTeam"
+            >Команда проекта</a
+          >
+          <table class="team__table" v-if="isTeamVisible">
+            <tr v-for="(item, i) in team" :key="i">
+              <td class="team__cell team__cell_right">{{ item.position }}</td>
+              <td class="team__cell">{{ item.fullName }}</td>
+            </tr>
+          </table>
+        </div>
+
         <copyright></copyright>
       </div>
     </div>
@@ -37,7 +82,9 @@
 </template>
 
 <script>
-import { computed, defineComponent } from 'vue';
+import {
+  computed, defineComponent, reactive, ref,
+} from 'vue';
 import { useStore } from 'vuex';
 import { useRouter } from 'vue-router';
 import Labels from '../components/Labels.vue';
@@ -49,6 +96,26 @@ export default defineComponent({
   setup() {
     const store = useStore();
     const router = useRouter();
+    const isTeamVisible = ref(false);
+    const team = reactive([
+      { position: 'Креатив', fullName: 'Сергей Габер' },
+      { position: 'Редактор', fullName: 'Маша Пчелкина' },
+      { position: 'Автор', fullName: 'Тоня Рубцова' },
+      { position: 'Дизайнер', fullName: 'Таня Репина' },
+      { position: 'Разработчик', fullName: 'Григорий Тимошук' },
+      { position: 'Тестировщик', fullName: 'Ксения Старостина' },
+      { position: 'Аналитик', fullName: 'Валерия Нечаева' },
+      { position: 'Менеджер проекта', fullName: 'Яна Скрипичникова' },
+      { position: 'Менеджер по продажам', fullName: 'Галина Корецкая' },
+    ]);
+
+    const showTeam = () => {
+      isTeamVisible.value = true;
+    };
+
+    const hideTeam = () => {
+      isTeamVisible.value = false;
+    };
 
     const tryAgain = () => {
       store.commit('resetUserProgress');
@@ -60,7 +127,11 @@ export default defineComponent({
     return {
       resultPromo: store.state.resultPromo,
       result,
+      team,
+      isTeamVisible,
       tryAgain,
+      showTeam,
+      hideTeam,
     };
   },
 });
