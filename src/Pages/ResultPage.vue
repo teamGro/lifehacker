330b96ced3b2db2b5dd1dd01d2079b819307a07f
TG <tima.groshev@yandex.ru>
2021-11-15 16:11:20 +0300

    fixed styles for result page

--- src/Pages/ResultPage.vue
+++ src/Pages/ResultPage.vue
@@ -1,14 +1,15 @@
 <template>
+  <labels></labels>
+  <div class="shadow"></div>
   <main class="page page_result result">
     <div
       class="page__header"
       :style="{ backgroundImage: `url('${result.image}')` }"
-    >
-      <labels></labels>
-    </div>
-
+    ></div>
     <div class="result__body">
-      <section class="section result__section">
+      <section
+        class="page__section container result__section result__section_top"
+      >
         <h2 class="result__title">{{ result.title }}</h2>
         <p class="result__text">{{ result.text }}</p>
         <div class="result__btns-wrapper">
@@ -19,14 +20,18 @@
           </button>
         </div>
       </section>
-      <section class="section result__section result__section_bottom">
-        <p class="result__promo">{{ resultPromo }}</p>
-        <a href="#" class="btn result__buy-btn">Купить шины со скидкой</a>
-        <div class="result__footer">
-          <a href="#" class="link result__team">Команда проекта</a>
-          <copyright></copyright>
+      <section
+        class="page__section container result__section result__section_bottom"
+      >
+        <p class="result__promo" v-html="resultPromo"></p>
+        <div class="result__buy-btn">
+          <a href="#" class="btn result__btn">Купить шины со скидкой</a>
         </div>
       </section>
+      <div class="result__footer container">
+        <a href="#" class="link result__team">Команда проекта</a>
+        <copyright></copyright>
+      </div>
     </div>
   </main>
 </template>
@@ -62,117 +67,4 @@ export default defineComponent({
 </script>
 
 <style lang="less">
-.result {
-  border-bottom: 2px solid #fff;
-
-  &__title {
-    margin-bottom: 16px;
-
-    font-family: "Play";
-    font-weight: bold;
-    font-size: 35px;
-    line-height: 40px;
-  }
-
-  &__text {
-    margin-bottom: 40px;
-
-    font-weight: bold;
-    font-size: 18px;
-    line-height: 26px;
-  }
-
-  &__section {
-    display: flex;
-    flex-direction: column;
-  }
-
-  &__btns-wrapper {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-bottom: 27px;
-  }
-
-  &__social {
-    margin-bottom: 27px;
-  }
-
-  &__again-btn {
-    display: flex;
-    align-items: center;
-
-    color: #7dbfff;
-    background-color: transparent;
-  }
-
-  &__again-icon {
-    margin-right: 16px;
-    width: 40px;
-    height: 35px;
-    background-image: url("../assets/img/retry.png");
-    background-repeat: no-repeat;
-    background-position: center;
-  }
-
-  &__promo {
-    margin-bottom: 40px;
-
-    font-size: 16px;
-    line-height: 24px;
-  }
-
-  &__buy-btn {
-    margin-bottom: 35px;
-    padding: 16px 20px;
-
-    text-align: center;
-  }
-
-  &__team {
-    margin-bottom: 40px;
-
-    text-align: center;
-    font-size: 12px;
-    line-height: 15px;
-  }
-
-  &__footer {
-    padding-bottom: 25px;
-    display: flex;
-    flex-direction: column;
-  }
-}
-
-@media (min-width: 1280px) {
-  .result {
-    &__body {
-      width: 50%;
-    }
-
-    &__section {
-      padding-top: 120px !important;
-      width: 100% !important;
-      border-bottom: 2px solid #fff;
-
-      &_bottom {
-        padding-top: 25px !important;
-      }
-    }
-
-    &__btns-wrapper {
-      flex-direction: row;
-    }
-
-    &__social {
-      order: 1;
-      margin-bottom: 0;
-      margin-left: 52px;
-    }
-
-    &__buy-btn {
-      width: auto;
-    }
-  }
-}
 </style>
